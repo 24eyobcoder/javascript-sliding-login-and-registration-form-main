4d405eda9740e57732b1c2747b2c184c0625f97b
eyob million <eyobmillion425@gmail.com>
2024-06-12 11:48:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+
+const name_field = document.querySelector(".name-field");
+const password_field = document.querySelector(".password-field");
+const sign_btn = document.querySelector(".sign-btn");
+
+function saveName(e) {
+    localStorage.setItem('name', e.target.value);
+}
+
+function signIn(e) {
+    e.preventDefault(); // Prevent form submission
+    window.location.href = '/display.html'; // Redirect to the details page
+}
+
+password_field.addEventListener("input", saveName); // Listen for input event
+sign_btn.addEventListener("click", signIn); // Listen for click event
+
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
+});
